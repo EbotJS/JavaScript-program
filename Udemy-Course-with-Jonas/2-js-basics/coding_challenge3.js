45fb7a403f1cf2ff9dfb9ac42ac65aa2d2963c37
EbotJS <achaleebotoma2002@gmail.com>
2021-11-26 21:52:51 +0100

    just completed a coding challenge under a Udemy course

---
+++ Udemy-Course-with-Jonas/2-js-basics/coding_challenge3.js
@@ -0,0 +1,104 @@
+// function simpleTipCalculator(bill1, bill2, bill3) {
+//     var tips = [], bills=[], tip1, tip2, tip3;
+    
+//     // bill 1
+    
+//     if(bill1 < 50){
+//          tip1= bill*(20/100);
+//          tips.push(tip1);
+//          bills.push(bill1 + tip1);
+//      }
+//      else if(bill1> 50 && bill1 < 200){
+//          tip1= bill1 * (15/100);
+//          tips.push(tip1);
+//          bills.push(bill1 + tip1);
+//      }
+//      else if(bill1 > 200){
+//          tip1= bill1*(10/100);
+//          tips.push(tip1);
+//          bills.push(bill1 + tip1);
+//      }
+
+//      //bill 2
+//      if(bill2 < 50){
+//         tip2= bill2 *(20/100);
+//         tips.push(tip2);
+//         bills.push(bill2 + tip2);
+//     }
+//     else if(bill2> 50 && bill2 < 200){
+//         tip2= bill2 * (15/100);
+//         tips.push(tip2);
+//         bills.push(bill2 + tip2);
+//     }
+//     else if(bill2 > 200){
+//         tip2= bill2 *(10/100);
+//         tips.push(tip2);
+//         bills.push(bill2 + tip2);
+//     }
+
+//     //bill 3
+
+//     if(bill3 < 50){
+//         tip3= bill3 *(20/100);
+//         tips.push(tip3);
+//         bills.push(bill3 + tip3);
+//     }
+//     else if(bill3> 50 && bill3 < 200){
+//         tip3= bill3 * (15/100);
+//         tips.push(tip3);
+//         bills.push(bill3 + tip3);
+//     }
+//     else if(bill3 > 200){
+//         tip3= bill3*(10/100);
+//         tips.push(tip3);
+//         bills.push(bill3 + tip3);
+//     }
+// return `bills: ${bills}
+// tips: ${tips}`;
+// }
+
+// console.log(simpleTipCalculator(124, 48, 268));
+
+
+
+
+/*******************************************************
+ * THIS IS MY UPDATED PROGRAM(This is my solution from my own point of view)
+ */
+
+var bills = [124, 48, 268];
+
+function simpleTipCalculator(bill){
+
+var finalPaidBill = [];
+var finalTips = [];
+var tip = [];
+
+    for(let i=0; i<3; i++){
+
+        if(bill[i] < 50){
+
+            tip[i]= bill[i] *(20/100);
+            finalTips.push(tip[i]);
+            finalPaidBill.push(bill[i] + tip[i]);
+
+        }else if(bill[i]> 50 && bill[i] < 200){
+            
+            tip[i]= bill[i] * (15/100);
+            finalTips.push(tip[i]);
+            finalPaidBill.push(bill[i] + tip[i]);
+        }
+        else if(bill[i] > 200){
+            tip[i]= bill[i] * (10/100);
+            finalTips.push(tip[i]);
+            finalPaidBill.push(bill[i] + tip[i]);
+        }
+    }
+    return `bills: ${finalPaidBill}
+tips: ${finalTips}`;
+ 
+}
+
+console.log(simpleTipCalculator(bills));
+
+
--- Udemy-Course-with-Jonas/2-js-basics/script.js
+++ Udemy-Course-with-Jonas/2-js-basics/script.js
@@ -310,6 +310,7 @@ console.log(whatDoYouDo('driver', 'Ebot'));
  */
 
 // Initialize new array
+/*
 var names = ['John', 'Mark', 'Jane'];
 var years = new Array(1990, 1967, 3458);
 
@@ -337,3 +338,34 @@ console.log(john.indexOf(23));
 
 var isDesigner = john.indexOf('designer') === -1 ? 'John is Not a designer' : 'John is a designer';
 console.log(isDesigner);
+*/
+
+/*****************************
+ * vid 023 Coding Challenge 3
+ */
+
+// JONAS' SOLUTION
+
+function tipCalculator(bill) {
+    var percentage;
+    if(bill < 50) {
+        percentage = .2;
+    }else if(bill >= 50 && bill < 200) {
+        percentage = .15;
+    }else{
+        percentage = .1;
+    }
+    return percentage * bill;
+}
+
+var bills = [124, 48, 268];
+var tips = [tipCalculator(bills[0]),
+            tipCalculator(bills[1]),
+            tipCalculator(bills[2])];
+
+var finalValues = [bills[0] + tips[0],
+                   bills[1] + tips[1],
+                   bills[2] + tips[2]];
+
+
+console.log(tips, finalValues);
